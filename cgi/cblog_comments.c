eef1e907c9bf320ef63df26230b53512ccaa4f2b
Freddy DISSAUX <freddy.dsx@free.fr>
2010-02-17 19:17:22 +0000

    style

--- cgi/cblog_comments.c
+++ cgi/cblog_comments.c
@@ -51,7 +51,7 @@ get_comments(HDF *hdf, char *postname)
 
 	while (fgets(filebuf, LINE_MAX, comment) != NULL) {
 		if (STARTS_WITH(filebuf, "comment: "))
-			hdf_set_valuef(hdf, "Posts.0.comments.%i.content=%s", count, cgi_url_unescape(filebuf + 9 ));
+			hdf_set_valuef(hdf, "Posts.0.comments.%i.content=%s", count, cgi_url_unescape(filebuf + 9));
 
 		else if (STARTS_WITH(filebuf, "name: "))
 			hdf_set_valuef(hdf, "Posts.0.comments.%i.author=%s", count, filebuf + 6);
@@ -64,7 +64,7 @@ get_comments(HDF *hdf, char *postname)
 			comment_date = (time_t)strtol(filebuf + 6, NULL, 10);
 			time_to_str(comment_date, date_format, date, 256);
 			hdf_set_valuef(hdf, "Posts.0.comments.%i.date=%s", count, date);
-		}else if (STARTS_WITH(filebuf, "--"))
+		} else if (STARTS_WITH(filebuf, "--"))
 			count++;
 	}
 
@@ -81,62 +81,61 @@ set_comment(HDF *hdf, char *postname)
 	FILE	*comment_fd;
 
 	/* very simple antispam */
-	if (strlen(get_query_str(hdf,"test1")) != 0)
+	if (strlen(get_query_str(hdf, "test1")) != 0)
 		return;
 
 	/* second one just in case */
 	if ((nospam = hdf_get_value(hdf, "antispamres", NULL)) == NULL)
 		return;
 
-	if (get_query_str(hdf,"antispam") == NULL)
+	if (get_query_str(hdf, "antispam") == NULL)
 		return;
 
-	if (!EQUALS(nospam,get_query_str(hdf,"antispam")))
+	if (!EQUALS(nospam, get_query_str(hdf, "antispam")))
 		return;
 
 	/* prevent empty name and empty comment */
-	if (strlen(get_query_str(hdf,"name")) == 0 || strlen(get_query_str(hdf,"comment")) == 0)
+	if (strlen(get_query_str(hdf, "name")) == 0 || strlen(get_query_str(hdf, "comment")) == 0)
 		return;
 
 	snprintf(comment_file, MAXPATHLEN, CDB_PATH"/comments/%s", postname);
 
-	comment_fd = fopen(comment_file,"a");
+	comment_fd = fopen(comment_file, "a");
 
 	/* TODO Log here */
 
 	if (comment_fd == NULL)
 		return;
 
-	cgi_url_escape( get_query_str(hdf,"comment"), &comment);
+	cgi_url_escape(get_query_str(hdf, "comment"), &comment);
 	fprintf(comment_fd, "comment: %s\n", comment);
-	fprintf(comment_fd, "name: %s\n", get_query_str(hdf,"name"));
-	fprintf(comment_fd, "url: %s\n", get_query_str(hdf,"url"));
-	fprintf(comment_fd, "date: %lld\n", (long long int) time(NULL));
-	fprintf(comment_fd, "ip: %s\n", get_cgi_str(hdf,"RemoteAddress"));
+	fprintf(comment_fd, "name: %s\n", get_query_str(hdf, "name"));
+	fprintf(comment_fd, "url: %s\n", get_query_str(hdf, "url"));
+	fprintf(comment_fd, "date: %lld\n", (long long int)time(NULL));
+	fprintf(comment_fd, "ip: %s\n", get_cgi_str(hdf, "RemoteAddress"));
 	fprintf(comment_fd, "-----\n");
 
 	fclose(comment_fd);
 
 	/* All is good, send an email for the new comment */
-	if(hdf_get_int_value(hdf, "email.enable", 0) == 1)
-	{
+	if (hdf_get_int_value(hdf, "email.enable", 0) == 1) {
 		from = hdf_get_value(hdf, "email.from", NULL);
 		to   = hdf_get_value(hdf, "email.to", NULL);
 
-		snprintf(subject, LINE_MAX, "New comment by %s", get_query_str(hdf,"name"));
+		snprintf(subject, LINE_MAX, "New comment by %s", get_query_str(hdf, "name"));
 
-		if(from && to)
+		if (from && to)
 			send_mail(from, to, subject,
-					get_cgi_str(hdf,"RemoteAddress"),
-					get_query_str(hdf,"comment"),
-					get_query_str(hdf,"name"),
+					get_cgi_str(hdf, "RemoteAddress"),
+					get_query_str(hdf, "comment"),
+					get_query_str(hdf, "name"),
 					get_query_str(hdf, "url"));
 
 
 	}
 
 	/* Some cleanup on the hdf */
-	hdf_remove_tree(hdf,"Query.name");
-	hdf_remove_tree(hdf,"Query.url");
-	hdf_remove_tree(hdf,"Query.comment");
+	hdf_remove_tree(hdf, "Query.name");
+	hdf_remove_tree(hdf, "Query.url");
+	hdf_remove_tree(hdf, "Query.comment");
 }
