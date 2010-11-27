9a8a9a5223d88b5ebfb834a85e451ef77434dd5d
bapt@FreeBSD.org <bapt@FreeBSD.org>
2010-11-27 17:31:02 +0000

    cblog can now launch fastcgi it self : cblog.cgi (unix|tcp):(IP|PATH)[:port]

--- cgi/main.c
+++ cgi/main.c
@@ -3,10 +3,21 @@
 #include <fcgi_stdio.h>
 #include <syslog.h>
 #include <signal.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <netdb.h>
 
 #include "cblog_cgi.h"
 
+#ifndef UNIX_PATH_MAX
+#define UNIX_PATH_MAX 108
+#endif
+
 HDF *conf;
+int fd;
+char *unix_sock_path = NULL;
 
 static char *mandatory_config[] = {
 	"title",
@@ -86,6 +97,91 @@ write_cb(void *ptr, const char *data, int size)
 	return FCGI_fwrite((void *)data, sizeof(char), size, FCGI_stdout);
 }
 
+static void
+close_socket(int dummy) {
+	close(fd);
+	if (unix_sock_path != NULL)
+		unlink(unix_sock_path);
+
+	exit(0);
+}
+
+static int
+bind_socket(char *url) {
+	char *p = url;
+	char *port;
+	int er;
+	struct sockaddr_un un;
+	struct addrinfo *ai;
+	struct addrinfo hints;
+
+	if (!strncmp(p, "unix:", sizeof("unix:") - 1)) {
+		p += sizeof("unix:") - 1;
+
+		if (strlen(p) >= UNIX_PATH_MAX)
+			cblog_err(-1, "Socket path too long, exceeds %d characters\n", UNIX_PATH_MAX);
+		
+		memset(&un, 0, sizeof(struct sockaddr_un));
+		if ((fd = socket(AF_UNIX, SOCK_STREAM, 0)) == -1)
+			err(-1, "socket()");
+
+		un.sun_family = AF_UNIX;
+		strcpy(un.sun_path, p);
+		unix_sock_path = p;
+
+		if (setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, (int[]){1}, sizeof(int)) < 0)
+			err(1, "setsockopt()");
+
+		if (bind(fd, (struct sockaddr *) &un, sizeof(struct sockaddr_un)) == -1)
+			err(-1, "bind()");
+
+	} else if (!strncmp(p, "tcp:", sizeof("tcp:") - 1)) {
+		p += sizeof("tcp:") - 1;
+
+		port = strchr(p, ':');
+		if (!port)
+			cblog_err(-1, "invalid url\n");
+
+		port[0] = '\n';
+		port++;
+
+		memset(&hints, 0, sizeof hints);
+
+		hints.ai_family = PF_UNSPEC;
+		hints.ai_flags = AI_PASSIVE;
+		hints.ai_socktype = SOCK_STREAM;
+		if ((er = getaddrinfo(p, port, &hints, &ai)) != 0)
+			cblog_err(-1, "getaddrinfo(): %s", gai_strerror(er));
+
+		if ((fd = socket(ai->ai_family, ai->ai_socktype, ai->ai_protocol)) == -1)
+			err(-1, "socket()");
+
+		if (setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, (int[]){1}, sizeof(int)) < 0)
+			err(1, "setsockopt()");
+
+		if (bind(fd, ai->ai_addr, ai->ai_addrlen) == -1)
+			err(-1, "bind()");
+
+	}
+
+	signal(SIGINT, close_socket);
+	signal(SIGKILL, close_socket);
+	signal(SIGQUIT, close_socket);
+	signal(SIGTERM, close_socket);
+
+
+	if (listen(fd, 1024) < 0)
+		err(1, "listen()");
+
+	if (dup2(fd, 0) < 0)
+		err(-1, "dup2()");
+
+	if (close(fd) < 0)
+		err(-1, "close()");
+
+	return 0;
+}
+
 /* end of fcgi wrappers */
 
 int
@@ -119,11 +215,15 @@ main(int argc, char **argv, char **envp)
 	cgiwrap_init_emu(NULL, &read_cb, &writef_cb, &write_cb,
 		NULL, NULL, NULL);
 	cgiwrap_init_std(argc, argv, envp);
+	if (argc == 2) {
+/*		daemon(0,0);*/
+		bind_socket(argv[1]);
+	}
 
 	while (FCGI_Accept() >= 0) {
 		/*	cgi_init(&cgi, NULL);
 		cgi_parse(cgi); */
-		cblogcgi(conf); 
+		cblogcgi(conf);
 		/*	cgi_destroy(&cgi);
 		syslog(LOG_ERR, "coucou"); */
 	}
