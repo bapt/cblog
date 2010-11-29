ca4985b3b8244bf337ddd849820d108659164b12
bapt@FreeBSD.org <bapt@FreeBSD.org>
2010-11-29 11:18:04 +0000

    release 0.1.5

--- lib/cblog_common.h
+++ lib/cblog_common.h
@@ -1,7 +1,7 @@
 #ifndef	CBLOG_LIB_COMMON_H
 #define	CBLOG_LIB_COMMON_H
 
-static char *cblog_version = "CBlog 0.1.4";
+static char *cblog_version = "CBlog 0.1.5";
 static char *cblog_url = "http://git.etoilebsd.net/cblog";
 
 static char *field[] = {
