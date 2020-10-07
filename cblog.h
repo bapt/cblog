7d85b60fe252708e737f9d1dfc248cdad0a642c7
Baptiste Daroussin <bapt@FreeBSD.org>
2020-10-07 22:27:58 +0200

    Remove unused macro

--- cblog.h
+++ cblog.h
@@ -13,11 +13,6 @@ static char *cblog_url = "https://github.com/bapt/cblog/";
 #define EQUALS(string, needle) (strcmp(string, needle) == 0)
 #define STARTS_WITH(string, needle) (strncasecmp(string, needle, strlen(needle)) == 0)
 
-#define HDF_FOREACH(var, hdf, node)		    \
-    for ((var) = hdf_get_child((hdf), node);	    \
-	    (var);				    \
-	    (var) = hdf_obj_next((var)))
-
 #define DEFAULT_POSTS_PER_PAGES 5
 
 #define CBLOG_GEN_CMD 0
